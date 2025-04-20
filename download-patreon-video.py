c1595bd8c14bf4602afab358d71433c94f8d1332
Manuel Fostier <manuel.fostier@gmail.com>
2025-04-20 23:40:35 +0200

    Utilisation du fichier cookies au lieu de chercher à lire les cookies du navigateur. telechargement et fusion la meilleur qualité video et audio

--- download-patreon-video.py
+++ download-patreon-video.py
@@ -4,23 +4,13 @@ import re
 
 def download_patreon_video(post_id):
     URL = f"https://www.patreon.com/posts/{post_id}"
-    yt_dlp_command = ["yt-dlp", "--cookies-from-browser", "chrome", URL, "-F"]
-    
-    output = subprocess.check_output(yt_dlp_command).decode('utf-8')
-    
-    video_resolutions = re.findall(r"(\d+)\s+(mp4\s+\d+x\d+)", output)
-    
-    print("Available resolutions: ")
-    for i, res in enumerate(video_resolutions, start=1):
-        print(f"{i}. {res[1]}")
-    
-    choice = int(input("Select resolution: "))
-    resolution_id = video_resolutions[choice-1][0]
+
+    resolution_id = "bv+ba/b"
     
     # Specify output directory and filename
     output_path = "~/patreon/%(title)s.%(ext)s"
     
-    download_command = ["yt-dlp", "--cookies-from-browser", "chrome", URL, "-f", resolution_id, "-o", output_path]
+    download_command = ["yt-dlp", "--cookies", "cookies.txt", URL, "-f", resolution_id, "-o", output_path]
     subprocess.run(download_command)
 
 if __name__ == "__main__":
